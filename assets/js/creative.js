b6fe45ce24396c7bc456a84ac6f91620d14853e7
acabey <17acabey@gmail.com>
2017-03-10 12:43:58 +0000

    Updated resume. Updated portolio photos

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -62,3 +62,15 @@
     });
 
 })(jQuery); // End of use strict
+
+$(document).ready(function() {
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+        });
+    });
