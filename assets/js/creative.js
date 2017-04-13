6b1c33837c67293459d4523caa00736de5d6e774
acabey <17acabey@gmail.com>
2017-04-13 00:49:42 +0000

    Renovated portfolio Removed stock photos and added details to descriptions

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -46,7 +46,7 @@
     }, 300);
 
     // Initialize and Configure Magnific Popup Lightbox Plugin
-    $('.popup-gallery').magnificPopup({
+    $('#popup-gallery-1').magnificPopup({
         delegate: 'a',
         type: 'image',
         tLoading: 'Loading image #%curr%...',
@@ -57,7 +57,70 @@
             preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: 'title',
+            titleSrc: function(item) {
+                return '' +
+                '<p class="text text-center">' + 
+                    'Lead Programmer for FIRST Team 1757 through the most ' + 
+                    '<a href="https://twitter.com/WWRobotics1757/status/850465241259487234"> ' +
+                        'successfull season' + 
+                    '</a>' +
+                    ' in the team\'s history.\n' +
+                    'Innovated new methods of ' + 
+                    '<a href="https://www.youtube.com/watch?v=YrOXxlsnZ70">' +
+                        'robot control ' + 
+                    '</a>' +
+                    'with PID-assisted driving and vision-processing backed targeting all implemented in ' +
+                    '<a href="https://github.com/1757WestwoodRobotics/2017-Steamworks">' +
+                        'Java programming' +
+                    '</a>' +
+                '</p>';
+            }
+        }
+    });
+    
+        $('#popup-gallery-2').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: 'title',
+            titleSrc: function(item) {
+                return '' + 
+                '<p class="text text-center">' +
+                    'Java teacher for supplementary education program, <a href="https://ussteam.org">USSTEAM Academy</a>' +
+                '</p>';
+            }
+        }
+    });
+    
+        $('#popup-gallery-3').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: 'title',
+            titleSrc: function(item) {
+                return '' + 
+                '<p class="text text-center">' +
+                    'Assembler for scratch-built computer engineering project, check out the code on <a href="https://github.com/acabey/LogiTimAssembler">Github</a>' +
+                '</p>';
+            }
         }
     });
 
